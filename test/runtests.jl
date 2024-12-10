bc2a2034cfbc29d3c08e574c7f62616d7b3340d7
Lukas Devos <ldevos98@gmail.com>
2024-12-10 14:29:19 -0500

    Skeleton updates (#2)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,61 @@
-@eval module $(gensym())
-using NestedPermutedDimsArrays: NestedPermutedDimsArray
-using Test: @test, @testset
-@testset "NestedPermutedDimsArrays" for elt in (
-  Float32, Float64, Complex{Float32}, Complex{Float64}
+using SafeTestsets: @safetestset
+using Suppressor: Suppressor
+
+# check for filtered groups
+# either via `--group=ALL` or through ENV["GROUP"]
+const pat = r"(?:--group=)(\w+)"
+arg_id = findfirst(contains(pat), ARGS)
+const GROUP = uppercase(
+  if isnothing(arg_id)
+    get(ENV, "GROUP", "ALL")
+  else
+    only(match(pat, ARGS[arg_id]).captures)
+  end,
 )
-  a = map(_ -> randn(elt, 2, 3, 4), CartesianIndices((2, 3, 4)))
-  perm = (3, 1, 2)
-  p = NestedPermutedDimsArray(a, perm)
-  T = PermutedDimsArray{elt,3,perm,invperm(perm),eltype(a)}
-  @test typeof(p) === NestedPermutedDimsArray{T,3,perm,invperm(perm),typeof(a)}
-  @test size(p) == (4, 2, 3)
-  @test eltype(p) === T
-  for I in eachindex(p)
-    @test size(p[I]) == (4, 2, 3)
-    @test p[I] == permutedims(a[CartesianIndex(map(i -> Tuple(I)[i], invperm(perm)))], perm)
+
+"match files of the form `test_*.jl`, but exclude `*setup*.jl`"
+istestfile(fn) =
+  endswith(fn, ".jl") && startswith(basename(fn), "test_") && !contains(fn, "setup")
+"match files of the form `*.jl`, but exclude `*_notest.jl` and `*setup*.jl`"
+isexamplefile(fn) =
+  endswith(fn, ".jl") && !endswith(fn, "_notest.jl") && !contains(fn, "setup")
+
+@time begin
+  # tests in groups based on folder structure
+  for testgroup in filter(isdir, readdir(@__DIR__))
+    if GROUP == "ALL" || GROUP == uppercase(testgroup)
+      for file in filter(istestfile, readdir(joinpath(@__DIR__, testgroup); join=true))
+        @eval @safetestset $file begin
+          include($file)
+        end
+      end
+    end
   end
-  x = randn(elt, 4, 2, 3)
-  p[3, 1, 2] = x
-  @test p[3, 1, 2] == x
-  @test a[1, 2, 3] == permutedims(x, invperm(perm))
-end
+
+  # single files in top folder
+  for file in filter(istestfile, readdir(@__DIR__))
+    (file == basename(@__FILE__)) && continue # exclude this file to avoid infinite recursion
+    @eval @safetestset $file begin
+      include($file)
+    end
+  end
+
+  # test examples
+  examplepath = joinpath(@__DIR__, "..", "examples")
+  for (root, _, files) in walkdir(examplepath)
+    contains(chopprefix(root, @__DIR__), "setup") && continue
+    for file in filter(isexamplefile, files)
+      filename = joinpath(root, file)
+      @eval begin
+        @safetestset $file begin
+          $(Expr(
+            :macrocall,
+            GlobalRef(Suppressor, Symbol("@suppress")),
+            LineNumberNode(@__LINE__, @__FILE__),
+            :(include($filename)),
+          ))
+        end
+      end
+    end
+  end
 end
