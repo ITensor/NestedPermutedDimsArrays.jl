42009d1f18c160fa70dd7fb31576bb78a469df82
mtfishman <7855256+mtfishman@users.noreply.github.com>
2025-11-11 01:49:18 +0000

    Format .jl files (Runic)

--- docs/make.jl
+++ docs/make.jl
@@ -2,23 +2,23 @@ using NestedPermutedDimsArrays: NestedPermutedDimsArrays
 using Documenter: Documenter, DocMeta, deploydocs, makedocs
 
 DocMeta.setdocmeta!(
-  NestedPermutedDimsArrays, :DocTestSetup, :(using NestedPermutedDimsArrays); recursive=true
+    NestedPermutedDimsArrays, :DocTestSetup, :(using NestedPermutedDimsArrays); recursive = true
 )
 
 include("make_index.jl")
 
 makedocs(;
-  modules=[NestedPermutedDimsArrays],
-  authors="ITensor developers <support@itensor.org> and contributors",
-  sitename="NestedPermutedDimsArrays.jl",
-  format=Documenter.HTML(;
-    canonical="https://itensor.github.io/NestedPermutedDimsArrays.jl",
-    edit_link="main",
-    assets=["assets/favicon.ico", "assets/extras.css"],
-  ),
-  pages=["Home" => "index.md", "Reference" => "reference.md"],
+    modules = [NestedPermutedDimsArrays],
+    authors = "ITensor developers <support@itensor.org> and contributors",
+    sitename = "NestedPermutedDimsArrays.jl",
+    format = Documenter.HTML(;
+        canonical = "https://itensor.github.io/NestedPermutedDimsArrays.jl",
+        edit_link = "main",
+        assets = ["assets/favicon.ico", "assets/extras.css"],
+    ),
+    pages = ["Home" => "index.md", "Reference" => "reference.md"],
 )
 
 deploydocs(;
-  repo="github.com/ITensor/NestedPermutedDimsArrays.jl", devbranch="main", push_preview=true
+    repo = "github.com/ITensor/NestedPermutedDimsArrays.jl", devbranch = "main", push_preview = true
 )
